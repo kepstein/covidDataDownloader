135eb59eaa49b7800dc85c016a80e341beaffee5
Kevin Epstein <kevin@epstein.co.za>
2020-03-28 19:48:41 -0700

    Full working version. serverless deploy is working correctly, and lambda function works as expected

--- main.py
+++ main.py
@@ -4,23 +4,12 @@ import urllib.request
 import boto3
 from botocore.exceptions import ClientError
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
 
 def download_data(url, file_name):
     urllib.request.urlretrieve(url, '/tmp/' + file_name)
 
 
 def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
     # If S3 object_name was not specified, use file_name
     if object_name is None:
         object_name = file_name
@@ -38,9 +27,13 @@ def upload_file(file_name, bucket, object_name=None):
 def lambda_handler(event, context):
     # Entry point for Lambda Execution
     download_data('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv', 'us-counties.csv')
+    print("Downloaded us-counties.csv file")
     download_data('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv', 'us-states.csv')
+    print("Downloaded us-states.csv file")
     upload_file('/tmp/us-states.csv', 'mybigdatabucket', 'covid19/nyt/states/us-states.csv')
-    upload_file('/tmp/us-counties', 'mybigdatabucket', 'covid19/nyt/counties/us-counties.csv')
+    print("Uploaded us-states.csv file to S3")
+    upload_file('/tmp/us-counties.csv', 'mybigdatabucket', 'covid19/nyt/counties/us-counties.csv')
+    print("Uploaded us-counties.csv file to S3")
 
 
 if __name__ == '__main__':
